fb84eded4c9eb48a5e9ec19c75e5aef47626395f
Maxim <Letushov@Maxims-MacBook-Pro.local>
2014-05-05 14:27:42 +0300

    Initial Commit

---
+++ IsEqualTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  IsEqualTest
+//
+//  Created by Maxim on 5/5/14.
+//  Copyright (c) 2014 Maxim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
